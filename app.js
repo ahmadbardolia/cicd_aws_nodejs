d2162637ad831540010453664e9777a4f517aa37
Ahmad H Bardolia <18185030+ahmadbardolia@users.noreply.github.com>
2022-07-20 23:12:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Edited the app.js from browser for new build'));
+app.get('/', (req, res) => res.send('Hello World! Edited the app.js from browser for new build using pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
